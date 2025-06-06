552c74882dbc1a06b750d946d1dbd073d8224d91
dhiaraayu <dhiara.yn@gmail.com>
2025-06-06 14:23:19 +0700

    Update 124240185_latresponsi.cpp

--- 124240185_latresponsi.cpp
+++ 124240185_latresponsi.cpp
@@ -28,7 +28,15 @@ struct Queue
   Queue *next;
 };
 
+struct Stack
+{
+  Spotify_KW data;
+  Stack *next;
+};
+
+// Global variables
 Queue *depan, *belakang;
+Stack *atas; // Pointer untuk top of stack
 
 void buatqueue()
 {
@@ -36,9 +44,41 @@ void buatqueue()
   belakang = NULL;
 }
 
+void buatstack()
+{
+  atas = NULL;
+}
+
 int queuekosong()
 {
   return (depan == NULL);
+}
+
+int stackkosong()
+{
+  return (atas == NULL);
+}
+
+void push_stack(Spotify_KW lagu)
+{
+  Stack *NB = new Stack;
+  NB->data = lagu;
+  NB->next = atas;
+  atas = NB;
+}
+
+Spotify_KW pop_stack()
+{
+  if (stackkosong())
+  {
+    return {"", "", ""};
+  }
+
+  Stack *hapus = atas;
+  Spotify_KW lagu_kembali = atas->data;
+  atas = atas->next;
+  delete hapus;
+  return lagu_kembali;
 }
 
 void tambah_lagu(Node *&root, Spotify_KW data)
@@ -133,6 +173,10 @@ void putar_lagu()
     cout << "Memutar lagu: " << depan->info << endl;
     Queue *hapus = depan;
     depan = depan->next;
+    if (depan == NULL) // Jika queue menjadi kosong
+    {
+      belakang = NULL;
+    }
     delete hapus;
   }
 }
@@ -145,21 +189,24 @@ Node *min_valueNode(Node *node)
   return current;
 }
 
-Node *hapus_lagu(Node *root, const string &judul)
+Node *hapus_lagu(Node *root, const string &judul, bool &found)
 {
   if (root == nullptr)
     return root;
 
   if (judul < root->data.judul)
   {
-    root->left = hapus_lagu(root->left, judul);
+    root->left = hapus_lagu(root->left, judul, found);
   }
   else if (judul > root->data.judul)
   {
-    root->right = hapus_lagu(root->right, judul);
+    root->right = hapus_lagu(root->right, judul, found);
   }
   else
   {
+    push_stack(root->data);
+    found = true;
+
     if (root->left == nullptr)
     {
       Node *temp = root->right;
@@ -172,11 +219,47 @@ Node *hapus_lagu(Node *root, const string &judul)
       delete root;
       return temp;
     }
-    Node *temp = minValueNode(root->right);
+    Node *temp = min_valueNode(root->right);
     root->data = temp->data;
-    root->right = hapus_lagu(root->right, temp->data.judul);
+    bool dummy = false;
+    root->right = hapus_lagu(root->right, temp->data.judul, dummy);
   }
   return root;
+}
+
+void undo_hapus_lagu(Node *&root)
+{
+  if (stackkosong())
+  {
+    cout << "Tidak ada lagu yang bisa dikembalikan!" << endl;
+  }
+  else
+  {
+    Spotify_KW lagu_kembali = pop_stack();
+    tambah_lagu(root, lagu_kembali);
+    cout << "Lagu \"" << lagu_kembali.judul << "\" berhasil dikembalikan!" << endl;
+  }
+}
+
+void lihat_antrian()
+{
+  if (queuekosong())
+  {
+    cout << "Antrian lagu kosong!" << endl;
+  }
+  else
+  {
+    cout << "Daftar antrian lagu:" << endl;
+    Queue *current = depan;
+    int nomor = 1;
+    while (current != nullptr)
+    {
+      cout << nomor << ". " << current->info << endl;
+      current = current->next;
+      nomor++;
+    }
+    cout << "-------------------------" << endl;
+  }
 }
 
 int main()
@@ -186,17 +269,20 @@ int main()
   string judul, penyanyi, album;
 
   buatqueue();
+  buatstack();
 
   do
   {
     system("cls");
-    cout << "===== SPOTIFY KW 1000 =====" << endl;
+    cout << "===== SPOTIFY KW 100 =====" << endl;
     cout << "[1] Tambah Data Lagu" << endl;
     cout << "[2] Pencarian Lagu" << endl;
     cout << "[3] Lihat Daftar Lagu" << endl;
     cout << "[4] Tambah Antrian Lagu" << endl;
     cout << "[5] Putar Lagu" << endl;
     cout << "[6] Hapus Lagu" << endl;
+    cout << "[7] Undo Hapus Lagu" << endl;
+    cout << "[8] Lihat Antrian" << endl;
     cout << "[0] Keluar" << endl;
     cout << "===========================" << endl;
     cout << "Pilih menu: ";
@@ -246,7 +332,7 @@ int main()
 
     case 3:
       system("cls");
-      cout << "----- Daftar Lagu -----" << endl;
+      cout << "----- Daftar Lagu (A-Z) -----" << endl;
       if (root == nullptr)
       {
         cout << "\nBelum ada lagu yang tersimpan" << endl;
@@ -269,7 +355,7 @@ int main()
       if (lagu_antrian != nullptr)
       {
         tambah_antrian(judul);
-        cout << "\nLagu berhasil ditambahkan ke antrian!" << endl;
+        cout << "\nLagu \"" << judul << "\" berhasil ditambahkan ke antrian!" << endl;
       }
       else
       {
@@ -288,17 +374,43 @@ int main()
       break;
 
     case 6:
+    {
       system("cls");
       cout << "----- Hapus Lagu -----" << endl;
       cout << "Judul lagu yang ingin dihapus: ";
       getline(cin, judul);
-      root = hapus_lagu(root, judul);
-      cout << "\nLagu berhasil dihapus" << endl;
+
+      bool found = false;
+      root = hapus_lagu(root, judul, found);
+
+      if (found)
+      {
+        cout << "\nLagu \"" << judul << "\" berhasil dihapus!" << endl;
+      }
+      else
+      {
+        cout << "\nLagu dengan judul \"" << judul << "\" tidak ditemukan!" << endl;
+      }
+      system("pause");
+      break;
+    }
+
+    case 7:
+      system("cls");
+      cout << "----- Undo Hapus Lagu -----" << endl;
+      undo_hapus_lagu(root);
+      system("pause");
+      break;
+
+    case 8:
+      system("cls");
+      cout << "----- Lihat Antrian -----" << endl;
+      lihat_antrian();
       system("pause");
       break;
 
     case 0:
-      cout << "Terima kasih telah menggunakan Spotify KW 1000 ^.^" << endl;
+      cout << "Terima kasih telah menggunakan Spotify KW 100 ^.^" << endl;
       break;
 
     default:
